cfd9375f32fcb20bc1efe5b8c1a83e65986769c7
Remya Panicker <mak.rem1545@gmail.com>
2021-11-24 23:42:03 +0100

    added elements via Javascript

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Quiz App");
 
-//for changing the background mode
+//for changing the background to light or dark mode
 let darkModeBtn = document.querySelector("#darkMode");
 let i = 0;
 
@@ -21,12 +21,175 @@ darkModeBtn.addEventListener("click", () => {
   }
 });
 
+//Array of Objects
+
+let questions = [
+  {
+    id: "Q1",
+    inputType: "radio",
+    question: "1 + 1 is 2",
+    options: [true, false],
+    answer: [true],
+  },
+  {
+    id: "Q2",
+    inputType: "radio",
+    question: "1 - 1 is 2",
+    options: [true, false],
+    answer: [false],
+  },
+  {
+    id: "Q3",
+    inputType: "radio",
+    question: "1 * 1 is 1",
+    options: [true, false],
+    answer: [true],
+  },
+  {
+    id: "Q4",
+    inputType: "checkBox",
+    question: "Question No.4",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
+    answer: ["Option 2", "Option 3"],
+  },
+  {
+    id: "Q5",
+    inputType: "dropDown",
+    question: "Question No.5",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: ["Option 3"],
+  },
+  {
+    id: "Q6",
+    inputType: "dropDown",
+    question: "Question No.6",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: ["Option 2"],
+  },
+  {
+    id: "Q7",
+    inputType: "dropDown",
+    question: "Question No.7",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: ["Option 1"],
+  },
+];
+
+//functions for creating the questions and options
+
+//radio buttons
+let radioButton = (question, values, name) => {
+  let heroQuestions = document.querySelector("#questions");
+  let questionH3 = document.createElement("h3");
+  questionH3.textContent = question;
+  heroQuestions.appendChild(questionH3);
+
+  values.forEach((value) => {
+    let questionInput = document.createElement("input");
+    questionInput.type = "radio";
+    questionInput.value = value;
+    questionInput.name = name;
+    heroQuestions.appendChild(questionInput);
+
+    let labelValue = document.createElement("label");
+    labelValue.innerHTML += questionInput.value;
+    heroQuestions.appendChild(labelValue);
+  });
+};
+//checkbox
+let checkBox = (question, values) => {
+  let heroQuestions = document.querySelector("#questions");
+  let questionH3 = document.createElement("h3");
+  questionH3.textContent = question;
+  heroQuestions.appendChild(questionH3);
+
+  values.forEach((value) => {
+    let questionInput = document.createElement("input");
+    questionInput.type = "checkbox";
+    questionInput.value = value;
+    questionInput.className = "checkboxOptions";  
+    heroQuestions.appendChild(questionInput);
+
+    let labelValue = document.createElement("label");
+    labelValue.innerHTML += questionInput.value;
+    heroQuestions.appendChild(labelValue);
+  });
+};
+
+//dropdowns
+let dropDown = (question, values) => {
+  let heroQuestions = document.querySelector("#questions");
+  let questionH3 = document.createElement("h3");
+  questionH3.textContent = question;
+  heroQuestions.appendChild(questionH3);
+
+  let selectForDropDown = document.createElement("select");
+  heroQuestions.appendChild(selectForDropDown);
+
+  values.forEach((value) => {
+    let questionInput = document.createElement("option");
+    questionInput.value = value;
+    questionInput.textContent = value;
+    selectForDropDown.appendChild(questionInput);
+  });
+};
+
+//printing to DOM
+
+questions.forEach((object) => {
+  if (object.inputType == "radio") {
+    radioButton(object.question, object.options, object.id);
+  } else if (object.inputType == "checkBox") {
+    checkBox(object.question, object.options);
+  } else if (object.inputType == "dropDown") {
+    dropDown(object.question, object.options);
+  }
+});
+
 //function for checking the results
 let submit = document.querySelector("#submitButton");
 
-submit.addEventListener("click", () => {
-  let correctAnswers = 0;
+//for radio buttons
+let RadioBtnCheck = ((correctAnswer, name) =>{
+  let userAnswer = document.querySelector(
+    `input[name='${name}']:checked`).value;
+  console.log(userAnswer);
+  if (userAnswer === correctAnswer) {
+    return 1;         
+  } return 0;
+}); 
 
+//for checkboxes
+let CheckBoxCheck = ((correctAnswer) =>{
+  let checkboxes = document.querySelectorAll(".checkboxOptions");
+  let userAnswers = [];
+
+  checkboxes.forEach((box) => {
+    if (box.checked) {
+      userAnswers.push(box.value);
+    }
+  });
+  console.log(userAnswers);
+  
+  if (correctAnswer[0] === userAnswers[0] && correctAnswer[1] === userAnswers[1]) {
+    return 1;
+  } return 0;
+}); 
+
+//add eventlistener
+submit.addEventListener("click", () => {
+  let totalScore = 0;
+  questions.forEach((object)=>{
+    if (object.inputType === "radio"){
+      totalScore = totalScore + RadioBtnCheck(object.answer, object.id);  }
+      // else if(object.inputType === "checkBox"){
+      //   totalScore = totalScore + CheckBoxCheck(object.answer) ;
+      // }
+       
+  });
+  console.log(totalScore);
+
+  //radio Buttons
   // let userAnswer1 = document.querySelector(
   //   "input[name='question1']:checked"
   // ).value;
@@ -51,22 +214,24 @@ submit.addEventListener("click", () => {
   //   correctAnswers++;
   // }
 
-  let checkboxes = document.querySelectorAll(".quest4Options");
+  // //checkBoxes
+  // let checkboxes = document.querySelectorAll(".quest4Options");
 
-  let corAnswers = ["Option 1", "Option 2"];
-  let userAnswers = [];
-  checkboxes.forEach((box) => {
-    if (box.checked) {
-      userAnswers.push(box.value);
-    }
-  });
-  console.log(userAnswers);
-  console.log(corAnswers);
-  if (corAnswers[0] == userAnswers[0] && corAnswers[1] == userAnswers[1]) {
-    console.log("true");
-    correctAnswers++;
-  }
+  // let corAnswers = ["Option 1", "Option 2"];
+  // let userAnswers = [];
+  // checkboxes.forEach((box) => {
+  //   if (box.checked) {
+  //     userAnswers.push(box.value);
+  //   }
+  // });
+  // console.log(userAnswers);
+  // console.log(corAnswers);
+  // if (corAnswers[0] == userAnswers[0] && corAnswers[1] == userAnswers[1]) {
+  //   console.log("true");
+  //   correctAnswers++;
+  // }
 
+  // //dropdown
   // let question5Options = document.querySelector("#question5List");
   // let userAnswer5 =
   //   question5Options.options[question5Options.selectedIndex].value;
@@ -88,7 +253,16 @@ submit.addEventListener("click", () => {
   //   correctAnswers++;
   // }
 
-  console.log(correctAnswers);
+  // console.log(countOfCorrectAnswers);
+
+  // let message = document.querySelector("#message");
+  // let messageTag = document.createElement("p");
+  // message.appendChild(messageTag);
+
+  // if (countOfCorrectAnswers == 7) {
+  //   messageTag.textContent =
+  //     "Congratulations!! You got all the answers right! Great work!";
+  // } else {
+  //   messageTag.textContent = "Your score is " + countOfCorrectAnswers + ".";
+  // }
 });
-
-
