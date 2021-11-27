3ba4851b8b0cf32adfa8ef0ef0ed27014511b5ce
Remya Panicker <mak.rem1545@gmail.com>
2021-11-27 10:17:17 +0100

    added message on Dom

--- main.js
+++ main.js
@@ -27,55 +27,55 @@ let questions = [
   {
     id: "Q1",
     inputType: "radio",
-    question: "1 + 1 is 2",
+    question: "1) 1 + 1 is 2",
     options: [true, false],
-    answer: [true],
+    answer: ["true"],
   },
   {
     id: "Q2",
     inputType: "radio",
-    question: "1 - 1 is 2",
+    question: "2) 1 - 1 is 2",
     options: [true, false],
-    answer: [false],
+    answer: ["false"],
   },
   {
     id: "Q3",
     inputType: "radio",
-    question: "1 * 1 is 1",
+    question: "3) 1 * 1 is 1",
     options: [true, false],
-    answer: [true],
+    answer: ["true"],
   },
   {
     id: "Q4",
     inputType: "checkBox",
-    question: "Question No.4",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
-    answer: ["Option 2", "Option 3"],
+    question: "4) Square root of 25",
+    options: ["25", "-5", "5", "-25", "none"],
+    answer: ["-5", "5"],
   },
   {
     id: "Q5",
     inputType: "dropDown",
-    question: "Question No.5",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: ["Option 3"],
+    question: "5) Sum of the angles of a triangle?",
+    options: ["180 degree", "360 degree", "90 degree"],
+    answer: ["180 degree"],
   },
   {
     id: "Q6",
     inputType: "dropDown",
-    question: "Question No.6",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: ["Option 2"],
+    question: "6) choose the right answer (4 – 5) – (13 – 18 + 2)?",
+    options: ["-2", "2", "-1"],
+    answer: ["2"],
   },
   {
     id: "Q7",
     inputType: "dropDown",
-    question: "Question No.7",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: ["Option 1"],
+    question: "7) Approximate value of pi ( π )  ",
+    options: ["22", "22.7", "3.14"],
+    answer: ["3.14"],
   },
 ];
 
-//functions for creating the questions and options
+//------functions for creating the questions and options------//
 
 //radio buttons
 let radioButton = (question, values, name) => {
@@ -107,7 +107,7 @@ let checkBox = (question, values) => {
     let questionInput = document.createElement("input");
     questionInput.type = "checkbox";
     questionInput.value = value;
-    questionInput.className = "checkboxOptions";  
+    questionInput.className = "checkboxOptions";
     heroQuestions.appendChild(questionInput);
 
     let labelValue = document.createElement("label");
@@ -117,14 +117,24 @@ let checkBox = (question, values) => {
 };
 
 //dropdowns
-let dropDown = (question, values) => {
+let dropDown = (question, values, name) => {
   let heroQuestions = document.querySelector("#questions");
   let questionH3 = document.createElement("h3");
   questionH3.textContent = question;
   heroQuestions.appendChild(questionH3);
 
   let selectForDropDown = document.createElement("select");
+  selectForDropDown.id = name;  
   heroQuestions.appendChild(selectForDropDown);
+
+  let firstOption = document.createElement("option");
+  firstOption.value = "firstOption";
+  firstOption.textContent = "Select answer";  
+  firstOption.style.visibility = "hidden";
+  //firstOption.style.visibility = "disabled";
+  // firstOption.style.visibility = "selected";
+  selectForDropDown.appendChild(firstOption);
+
 
   values.forEach((value) => {
     let questionInput = document.createElement("option");
@@ -142,127 +152,89 @@ questions.forEach((object) => {
   } else if (object.inputType == "checkBox") {
     checkBox(object.question, object.options);
   } else if (object.inputType == "dropDown") {
-    dropDown(object.question, object.options);
+    dropDown(object.question, object.options, object.id);
   }
 });
 
-//function for checking the results
-let submit = document.querySelector("#submitButton");
+//-----functions for checking the results-----//
+
 
 //for radio buttons
-let RadioBtnCheck = ((correctAnswer, name) =>{
+let RadioBtnCheck = (correctAnswer, name) => {
   let userAnswer = document.querySelector(
-    `input[name='${name}']:checked`).value;
-  console.log(userAnswer);
-  if (userAnswer === correctAnswer) {
-    return 1;         
-  } return 0;
-}); 
+    `input[name='${name}']:checked`
+  ).value;
+  
+  if (userAnswer == correctAnswer) {
+    return 1;
+  }
+  return 0;
+};
 
 //for checkboxes
-let CheckBoxCheck = ((correctAnswer) =>{
+let CheckBoxCheck = (correctAnswer) => {
   let checkboxes = document.querySelectorAll(".checkboxOptions");
   let userAnswers = [];
-
-  checkboxes.forEach((box) => {
+  checkboxes.forEach((box) => {    
     if (box.checked) {
       userAnswers.push(box.value);
     }
-  });
-  console.log(userAnswers);
-  
-  if (correctAnswer[0] === userAnswers[0] && correctAnswer[1] === userAnswers[1]) {
+    
+  }); console.log(userAnswers);
+
+  if (
+    correctAnswer.length === userAnswers.length &&
+    correctAnswer[0] === userAnswers[0] &&
+    correctAnswer[1] === userAnswers[1]
+  ) {
+    return 1;
+  }
+  return 0;
+};
+
+//for dropdown
+let DropDownCheck = (correctAnswer, name) =>{
+  let selectOptions = document.querySelector(`#${name}`);
+
+  let userAnswer = selectOptions.options[selectOptions.selectedIndex].value;
+  console.log(userAnswer + " : " + (userAnswer === correctAnswer));
+  if (userAnswer === correctAnswer) {
     return 1;
   } return 0;
-}); 
+};
 
-//add eventlistener
+//add eventlistener for get results button (Submit)
+let submit = document.querySelector("#submitButton");
 submit.addEventListener("click", () => {
   let totalScore = 0;
-  questions.forEach((object)=>{
-    if (object.inputType === "radio"){
-      totalScore = totalScore + RadioBtnCheck(object.answer, object.id);  }
-      // else if(object.inputType === "checkBox"){
-      //   totalScore = totalScore + CheckBoxCheck(object.answer) ;
-      // }
-       
+  questions.forEach((object) => {
+    if (object.inputType === "radio") {
+      totalScore = totalScore + RadioBtnCheck(object.answer[0], object.id);
+    } 
+    else if(object.inputType === "checkBox"){
+      totalScore = totalScore + CheckBoxCheck(object.answer) ;
+    } else if (object.inputType === "dropDown"){
+      totalScore = totalScore + DropDownCheck(object.answer[0], object.id);
+    }
+    console.log("each item's score " + totalScore);
   });
-  console.log(totalScore);
+  console.log(totalScore);  
 
-  //radio Buttons
-  // let userAnswer1 = document.querySelector(
-  //   "input[name='question1']:checked"
-  // ).value;
-  // console.log(userAnswer1);
-  // if (userAnswer1 === "True") {
-  //   correctAnswers++;
-  // }
+  //to get the message on DOM
 
-  // let userAnswer2 = document.querySelector(
-  //   "input[name='question2']:checked"
-  // ).value;
-  // console.log(userAnswer2);
-  // if (userAnswer2 === "False") {
-  //   correctAnswers++;
-  // }
+  let message = document.querySelector("#message");
+  let messageTag = document.createElement("p");
+  message.appendChild(messageTag);
 
-  // let userAnswer3 = document.querySelector(
-  //   "input[name='question3']:checked"
-  // ).value;
-  // console.log(userAnswer3);
-  // if (userAnswer3 === "True") {
-  //   correctAnswers++;
-  // }
-
-  // //checkBoxes
-  // let checkboxes = document.querySelectorAll(".quest4Options");
-
-  // let corAnswers = ["Option 1", "Option 2"];
-  // let userAnswers = [];
-  // checkboxes.forEach((box) => {
-  //   if (box.checked) {
-  //     userAnswers.push(box.value);
-  //   }
-  // });
-  // console.log(userAnswers);
-  // console.log(corAnswers);
-  // if (corAnswers[0] == userAnswers[0] && corAnswers[1] == userAnswers[1]) {
-  //   console.log("true");
-  //   correctAnswers++;
-  // }
-
-  // //dropdown
-  // let question5Options = document.querySelector("#question5List");
-  // let userAnswer5 =
-  //   question5Options.options[question5Options.selectedIndex].value;
-  // if (userAnswer5 === "Option 1") {
-  //   correctAnswers++;
-  // }
-
-  // let question6Options = document.querySelector("#question6List");
-  // let userAnswer6 =
-  //   question6Options.options[question6Options.selectedIndex].value;
-  // if (userAnswer6 === "Option 1") {
-  //   correctAnswers++;
-  // }
-
-  // let question7Options = document.querySelector("#question7List");
-  // let userAnswer7 =
-  //   question7Options.options[question7Options.selectedIndex].value;
-  // if (userAnswer7 === "Option 1") {
-  //   correctAnswers++;
-  // }
-
-  // console.log(countOfCorrectAnswers);
-
-  // let message = document.querySelector("#message");
-  // let messageTag = document.createElement("p");
-  // message.appendChild(messageTag);
-
-  // if (countOfCorrectAnswers == 7) {
-  //   messageTag.textContent =
-  //     "Congratulations!! You got all the answers right! Great work!";
-  // } else {
-  //   messageTag.textContent = "Your score is " + countOfCorrectAnswers + ".";
-  // }
+  if (totalScore == 7) {
+    messageTag.textContent =
+      "Congratulations! You got all the answers right! Your score is " + totalScore + ".";
+      messageTag.style.color = "green";
+  } else if (totalScore > (7/2)){
+    messageTag.textContent = "Your score is " + totalScore + ".";
+    messageTag.style.color = "orange";
+  } else {
+    messageTag.textContent = "Your score is " + totalScore + ".";
+    messageTag.style.color = "red";
+  }
 });
